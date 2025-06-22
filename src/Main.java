80f857ad343096933faa63365061958376675532
Shivam Upadhyay <workonlyshivam@gmail.com>
2025-06-22 07:36:01 +0530

    get integration
    implemented get functionality in newsfetcher and integrated in main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,12 +12,30 @@ public class Main {
         Scanner Input = new Scanner(System.in);
         String query = Input.nextLine();
         query=query.trim();
+        Properties prop = new Properties();
+        String apikey;
+        try{
+            FileInputStream fis = new FileInputStream("config.properties");
+            prop.load(fis);
+            apikey = prop.getProperty("API_KEY");
+        }catch (IOException e) {
+            System.out.println("Error loading config: " + e.getMessage());
+            return;
+        }
         if(query.isEmpty()==true) {
             System.out.println("Invalid input");
         }
         else {
             System.out.println("ok searching.........");
+            Newsfetcher fetch= new Newsfetcher();
+            try {
+                fetch.fetchnews(query, apikey);
+            } catch (Exception e) {
+                System.out.println("Error fetching news: " + e.getMessage());
+            }
+
         }
+
 
 
 
--- src/Newsfetcher.java
+++ src/Newsfetcher.java
@@ -1,2 +1,28 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 public class Newsfetcher {
+    public void fetchnews (String query,String apikey) throws Exception{
+        HttpClient Client =HttpClient.newBuilder().build();
+
+        String baseurl="https://newsapi.org/v2/everything";
+        String url = baseurl + "?q=" + query + "&apiKey=" + apikey;
+        URI uri= URI.create(url);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        HttpResponse<String> response = Client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+
+
+
+    }
+
+
+
 }
